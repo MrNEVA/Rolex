94fe2af5d116428c4f514173f2afeff9caa58cd7
MrNEVA <mrnevart@gmail.com>
2017-09-19 02:32:39 +0000

    rolex 95%

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -21,6 +21,12 @@ const newSeconds = seconds + (milliseconds / 1000)
 
 //Background
 
+ctx.strokeStyle = "#880099"
+ctx.lineWidth = 17
+ctx.lineCap = "round"
+ctx.shadowBlur = 15
+ctx.shadowColor = "#880099"
+
 //Hours
 ctx.fillStyle = "#333333"
 ctx.fillRect(0, 0, vw, vh)
@@ -30,17 +36,19 @@ ctx.stroke()
 
 //Minutes
 ctx.beginPath()
-ctx.arc(vw / 2, vh / 2, vw / 5, degToRad(270), degToRad(minutes * 6) -90)
+ctx.arc(vw / 2, vh / 2, vw / 4 -25, degToRad(270), degToRad(minutes * 6) -90)
 ctx.stroke()
-//Seconds5nPath()
+//SecondsPath()
 ctx.beginPath()
-ctx.arc(vw / 2, vh / 2, vw / 6, degToRad(270), degToRad(newSeconds * 6) -90)
+ctx.arc(vw / 2, vh / 2, vw / 4 -50, degToRad(270), degToRad(newSeconds * 6) -90)
 ctx.stroke()
 //Date
-
-
+ctx.font = "25px impact"
+ctx.fillStyle = "#880099"
+ctx.fillText(today, vw /2 - 65, (vh / 2) + 50)
 //Time
-
-
+ctx.font = "bold 35px impact"
+ctx.fillStyle = "#880099"
+ctx.fillText(time, vw /2 - 65, (vh / 2) + 20)
 
 }
